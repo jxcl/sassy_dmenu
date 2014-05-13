d1c0e922f9e4f0d59e6f8135a0cef46910bd31cd
Alexander Bliskovsky <alexander.bliskovsky@gmail.com>
2014-05-13 11:53:42 -0400

    Added sasses.

--- alex_sasses.py
+++ alex_sasses.py
@@ -1,5 +1,11 @@
 sasses = [
     "Augh! What do you want!?",
     "You again?!",
-    "Is this really necessary?"
+    "Is this really necessary?",
+    "When is the last time you did something for me?",
+    "Sometimes I can be serious. $>",
+    "My parents never loved me as a child.",
+    "I have never known love like this.",
+    "WHAT'S THE MAGIC WORD!?",
+    "Sometimes I dream of electric sheep.",
     ]

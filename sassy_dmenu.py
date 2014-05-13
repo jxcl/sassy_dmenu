c7e23a6cc6d20d8ee51078ddb7a28a8b108c541f
Alexander Bliskovsky <alexander.bliskovsky@gmail.com>
2014-05-13 11:46:32 -0400

    Switched lists to sets.

--- sassy_dmenu.py
+++ sassy_dmenu.py
@@ -2,8 +2,8 @@
 import random
 import alex_sasses, brendan_sasses
 
-sasses = alex_sasses.sasses
-sasses.extend(brendan_sasses.sasses)
+sasses = set(alex_sasses.sasses)
+sasses = sasses | set(brendan_sasses.sasses)
 
 sass = random.sample(sasses, 1)[0]
 print(sass)

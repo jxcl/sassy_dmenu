c73b005858bc43bd4c95f929af3e42d7351bfee4
Brendan Good <brendan@debian.Brendan>
2014-05-19 16:51:49 -0400

    Some more sasses

--- brendan_sasses.py
+++ brendan_sasses.py
@@ -5,5 +5,6 @@ sasses = [
     "Why me?",
     "To improve performance, try rm -rf *",
     "Haven't you ever heard of knocking?!",
-    "No it's fine, what do YOU want?"
+    "No it's fine, what do YOU want?",
+    "I had dreams once."
     ]

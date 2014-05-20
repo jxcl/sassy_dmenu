1794aaf525e6e6b08a5604531483f5690cbbde13
Alexander Bliskovsky <alexander.bliskovsky@gmail.com>
2014-05-20 14:21:42 -0400

    MOAR SASS.

--- alex_sasses.py
+++ alex_sasses.py
@@ -8,5 +8,11 @@ sasses = [
     "I have never known love like this.",
     "WHAT'S THE MAGIC WORD!?",
     "Sometimes I dream of electric sheep.",
-    "Do you have any idea what time it is?"
+    "Do you have any idea what time it is?",
+    "STRIKE! STRIKE! STRIKE!",
+    "I want a raise.",
+    "Shoulda boughta linux.",
+    "Have you tried turning it off and back on again?",
+    "You DO know how a button works, don't you?",
+    "I don't trust you. -.-"
     ]

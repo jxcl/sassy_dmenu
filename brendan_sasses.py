1f98bc9ce8f5ff159c7dfb39f247c30ff026769f
Brendan Good <brendan@debian.Brendan>
2014-05-28 15:07:52 -0400

    Moar sasses.

--- brendan_sasses.py
+++ brendan_sasses.py
@@ -6,5 +6,8 @@ sasses = [
     "To improve performance, try rm -rf *",
     "Haven't you ever heard of knocking?!",
     "No it's fine, what do YOU want?",
-    "I had dreams once."
+    "I had dreams once.",
+    "I think it's time you and me had a talk.",
+    "I'm worried about you.",
+    "Please stop."
     ]
